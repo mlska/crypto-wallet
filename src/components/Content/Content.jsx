8cbc82e90f39fbf6f83b60eb2a2d57b4a5c169ff
Kamil Malesa <kamil.malesa1@gmail.com>
2022-08-08 20:51:57 +0200

    added content

--- src/components/Content/Content.jsx
+++ src/components/Content/Content.jsx
@@ -1,15 +1,32 @@
-import React from "react";
+import React, { useContext } from "react";
 import bemCssModules from "bem-css-modules";
+import { Navigate, Route, Routes } from "react-router-dom";
 
 import { default as ContentStyles } from "./Content.scss";
 
-const styles = bemCssModules(ContentStyles);
+import { StoreContext } from "../../store/StoreProvider";
+
+const style = bemCssModules(ContentStyles);
+
+import Cryptos from "../Cryptos/Cryptos";
+import UserCryptos from "../UserCryptos/UserCryptos";
 
 const Content = () => {
+  const { user } = useContext(StoreContext);
+
+  const isUserLogged = Boolean(user);
+
   return (
-    <section className={styles()}>
-      <div>Content</div>
-    </section>
+    <main className={style()}>
+      <Routes>
+        <Route exact path="/" element={<Cryptos />} />
+        {isUserLogged && (
+          <Route exact path="/user-cryptos" element={<UserCryptos />} />
+        )}
+
+        <Route exact path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </main>
   );
 };
 
