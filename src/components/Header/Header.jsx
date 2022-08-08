6e6d91a7714a15930bedca3bb246b71227244941
Kamil Malesa <kamil.malesa1@gmail.com>
2022-08-08 20:52:27 +0200

    header design changes

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,26 +1,47 @@
-import React from "react";
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
 import bemCssModules from "bem-css-modules";
 import { default as HeaderStyles } from "./Header.scss";
+import { FaBitcoin } from "react-icons/fa";
 
-const styles = bemCssModules(HeaderStyles);
+import { StoreContext } from "../../store/StoreProvider";
+
+const style = bemCssModules(HeaderStyles);
 
 const Header = () => {
+  const { user } = useContext(StoreContext);
+
+  const isUserLogged = Boolean(user);
+
   return (
-    <header className={styles()}>
-      <div className={styles("logo-wrapper")}>Crypto Wallet</div>
-      <ul>
-        <li>
-          <a href="#">Rynek</a>
-        </li>
-        <li>
-          <a href="#">Portfel</a>
-        </li>
-      </ul>
-      <div className={styles("btngroup")}>
-        <button className={styles("btn", { secondary: true })}>
-          Logowanie
-        </button>
-        <button className={styles("btn")}>Rejestracja</button>
+    <header className={style()}>
+      <div className={style("content-wrapper")}>
+        <div className={style("logo")}>
+          <FaBitcoin style={{ color: "rgba(247,147,26)" }} />
+          Crypto Wallet
+        </div>
+        <ul>
+          <li>
+            <Link className={style("link")} to="/">
+              Rynek
+            </Link>
+          </li>
+          <li>
+            <Link className={style("link")} to="/user-cryptos">
+              Portfel
+            </Link>
+          </li>
+        </ul>
+        <div>
+          <button className={style("btn", { secondary: !isUserLogged })}>
+            {isUserLogged ? "Wyloguj" : "Logowanie"}
+          </button>
+          {isUserLogged ? (
+            ""
+          ) : (
+            <button className={style("btn")}>Rejestracja</button>
+          )}
+        </div>
       </div>
     </header>
   );
