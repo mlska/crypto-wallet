a58a09620487beb79688b17271e019ae2074f9a6
Kamil Malesa <kamil.malesa1@gmail.com>
2022-08-10 21:36:31 +0200

    added data fetch and save from localstorage

--- src/store/StoreProvider.jsx
+++ src/store/StoreProvider.jsx
@@ -7,9 +7,12 @@ export const StoreContext = createContext(null);
 const apiEndPoint =
   "/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=12&page=1&sparkline=false";
 
+const localStorageKey = "users";
+
 const StoreProvider = ({ children }) => {
   const [coins, setCoins] = useState([]);
   const [users, setUsers] = useState([]);
+  const [activeUser, setActiveUser] = useState(null);
 
   const getCoins = () => {
     axiosRequest
@@ -21,12 +24,29 @@ const StoreProvider = ({ children }) => {
       .catch((error) => console.log(error));
   };
 
+  const getLocalStorageUsers = () => {
+    const data = localStorage.getItem(localStorageKey);
+    const users = JSON.parse(data);
+    users && setUsers(users);
+  };
+
+  const setLocalStorageUsers = () => {
+    localStorage.setItem(localStorageKey, JSON.stringify(users));
+  };
+
   useEffect(() => {
     getCoins();
+    getLocalStorageUsers();
   }, []);
 
+  useEffect(() => {
+    setLocalStorageUsers();
+  }, [users]);
+
   return (
-    <StoreContext.Provider value={{ coins, setCoins, users, setUsers }}>
+    <StoreContext.Provider
+      value={{ coins, setCoins, users, setUsers, activeUser, setActiveUser }}
+    >
       {children}
     </StoreContext.Provider>
   );
